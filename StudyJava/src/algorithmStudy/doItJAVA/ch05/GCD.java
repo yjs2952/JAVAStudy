6b1c54d5c82259aa7fdebc51edfc5861b4c8240d
yjs <yjs2952@gmail.com>
2018-09-28 10:59:58 +0900

    알고리즘 GCD

---
+++ StudyJava/src/algorithmStudy/doItJAVA/ch05/GCD.java
@@ -0,0 +1,40 @@
+package algorithmStudy.doItJAVA.ch05;
+
+public class GCD {
+    public static int gcd(int x, int y) {
+
+        if (x % y == 0) {
+            return y;
+        } else {
+            return gcd(y, x % y);
+        }
+    }
+
+    public static int gcdLoop(int a, int b){
+        while (b > 0) {
+            int temp = a;
+            a = b;
+            b = temp % b;
+        }
+        return a;
+    }
+
+    public static int gcdArray(int[] a) {
+        int gcd = gcdLoop(a[0], a[1]);
+        for (int i = 2; i < a.length; i++) {
+            gcd = gcd(gcd, a[i]);
+        }
+        return gcd;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(gcd(6, 21));
+        System.out.println(gcdLoop(6, 21));
+
+        int[] gcdArray = new int[]{18, 24, 30, 22, 990};
+        //long start = System.nanoTime();
+        System.out.println(gcdArray(gcdArray));
+        //long end = System.nanoTime();
+        //System.out.println(end - start);
+    }
+}
