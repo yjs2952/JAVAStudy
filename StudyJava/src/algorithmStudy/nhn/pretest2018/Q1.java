43063e6ca5c31b14f03e22f132d0419d7ea7bf8c
yjs <yjs2952@gmail.com>
2018-09-29 17:34:41 +0900

    nhn 문제

--- StudyJava/src/algorithmStudy/nhn/pretest2018/Q1.java
+++ StudyJava/src/algorithmStudy/nhn/pretest2018/Q1.java
@@ -1,4 +1,57 @@
 package algorithmStudy.nhn.pretest2018;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Q1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int c = sc.nextInt();   // 카드 수
+        int p = sc.nextInt();   // 섞기 횟수
+        int[] n = new int[p];   // 제외할 카드 수
+
+        for (int i = 0; i < p; i++) {
+            n[i] = sc.nextInt();
+        }
+
+        shuffle(c, p, n);
+    }
+
+    public static void shuffle(int c, int p, int[] n) {
+        LinkedList<Integer> list = new LinkedList<>();
+        List<Integer> temp = new ArrayList<>();
+        for (int i = 1; i <= c; i++) {
+            list.add(i);
+        }
+
+        for (int i = 0; i < p; i++) {
+            int end = c;
+            while (true) {
+                int index = n[i];
+                for (int j = n[i]; j < end - n[i]; j++) {
+                    temp.add(list.get(j));
+                }
+
+                for (int j = n[i]; j < end - n[i]; j++) {
+                    list.remove(index);
+                }
+
+                for (int j = temp.size() - 1; j >= 0; j--) {
+                    list.addFirst(temp.get(j));
+                }
+
+                if (temp.size() <= n[i]) {
+                    temp.clear();
+                    break;
+                }
+                end = temp.size();
+                temp.clear();
+            }
+        }
+        for (int i = 0; i < 5; i++) {
+            System.out.println(list.get(i));
+        }
+    }
 }
--- StudyJava/src/algorithmStudy/nhn/pretest2018/Q2.java
+++ StudyJava/src/algorithmStudy/nhn/pretest2018/Q2.java
@@ -1,4 +1,24 @@
 package algorithmStudy.nhn.pretest2018;
 
+import java.util.Scanner;
+
 public class Q2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int rotDir = sc.nextInt();
+        String[][] arr = new String[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextLine();
+            }
+        }
+
+        solution(arr, rotDir);
+    }
+
+    public static void solution(String[][] arr, int rotDir) {
+
+    }
 }
